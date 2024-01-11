1935deddbb69f4b6b949afd38c08b925204abaf3
Octavio Lara <octavioasapchi@gmail.com>
2024-01-11 19:32:15 -0400

    ADD error view

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<div class="w-[90vw] mx-auto md:max-w-[732px] py-8 lg:max-w-[1000px]">
+  <h1 class="text-3xl font-bold text-center">No results found</h1>
+  <p class="text-center mt-4">Try searching for another word or phrase.</p>
+  <div class="flex justify-center">
+    <button class="bg-purple-500 text-white rounded p-2 mt-5">
+      <a href="/"> Volver al inicio para seguir buscando </a>
+    </button>
+  </div>
+</div>
